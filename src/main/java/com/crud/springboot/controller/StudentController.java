ba8bbce4d96f589685d06c296a83f2d0a98919ad
Nataraju, Tejus <TEJUS_NATARAJU@homedepot.com>
2021-01-19 10:55:36 -0500

    comments

--- src/main/java/com/crud/springboot/controller/StudentController.java
+++ src/main/java/com/crud/springboot/controller/StudentController.java
@@ -3,7 +3,6 @@ package com.crud.springboot.controller;
 import com.crud.springboot.model.Student;
 import com.crud.springboot.repository.StudentRepository;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
--- src/test/java/com/crud/springboot/SpringbootApplicationTests.java
+++ src/test/java/com/crud/springboot/SpringbootApplicationTests.java
@@ -1,13 +1,40 @@
 package com.crud.springboot;
 
 import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.junit4.SpringRunner;
 
+import org.springframework.test.web.servlet.MockMvc;
+
+import java.util.List;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+//import static org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+
+
+
+//For instance, we can use the @SpringBootTest annotation if we want the entire context to be created without starting the server.
+//
+//        With that in place, we can then add the @AutoConfigureMockMvc to inject a MockMvc instance and send HTTP requests:
 @SpringBootTest
+@RunWith(SpringRunner.class)
+@AutoConfigureMockMvc
 class SpringbootApplicationTests {
 
+//    StudentRepository studentRepository;
+
+    @Autowired
+    private MockMvc mvc;
+
     @Test
-    void contextLoads() {
+    void findAllStudents() throws Exception {
+       this.mvc.perform(get("/student")
+        .accept(MediaType.APPLICATION_JSON).contentType(MediaType.APPLICATION_JSON));
+
     }
 
 }
